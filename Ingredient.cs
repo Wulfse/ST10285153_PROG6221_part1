b4b28f46ab1c413058ace1f2a7c6c2c421c33e37
Daniel Andre Wulfse <130445313+Wulfse@users.noreply.github.com>
2024-04-16 17:33:42 +0200

    Update Ingredient.cs
    changed to updated finished code for ingredients class

--- Ingredient.cs
+++ Ingredient.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +6,25 @@ using System.Threading.Tasks;
 
 namespace PROG6221_part1
 {
-    // Class representing an ingredient in the recipe
+    /// <summary>
+    /// Class representing an ingredient in the recipe
+    /// </summary>
+    ///------------------------------------------------------------------------------------------------------------///
     public class Ingredient
     {
+        //getters and setters for ingredients( name, quantity and unit of measurement)
         public string Name { get; set; }
         public double Quantity { get; set; }
         public string Unit { get; set; }
+        //--------------------------------------------------------------------------------------------------------//
 
-        // Constructor
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="quantity"></param>
+        /// <param name="unit"></param>
+        ///------------------------------------------------------------------------------------------------------///
         public Ingredient(string name, double quantity, string unit)
         {
             Name = name;
@@ -22,3 +33,4 @@ namespace PROG6221_part1
         }
     }
 }
+//---------------------------------------------------------End of Class-------------------------------------------------------//
